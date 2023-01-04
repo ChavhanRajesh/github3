97840d24921eb927be02ad434426a171097fd3f7
ChavhanRajesh <chavhanrajesh697@gmail.com>
2023-01-04 17:41:26 +0530

    i pushed 2 codes please check and if any issue commit me back

---
+++ src/test/java/git3Test/demosub1.java
@@ -0,0 +1,7 @@
+package git3Test;
+
+public class demosub1 {
+public static void main(String[] args) {
+	System.out.println("king");
+}
+}
---
+++ src/test/java/git3Test/demosub2.java
@@ -0,0 +1,7 @@
+package git3Test;
+
+public class demosub2 {
+public static void main(String[] args) {
+	System.out.println("queen");
+}
+}
